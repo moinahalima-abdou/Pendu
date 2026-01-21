577e55f362f1518019a76bcd0f16962d49b34a5a
moinahalima-abdou <moina-halima.abdou-doukeini@laplateforme.io>
2026-01-21 14:16:59 +0100

    Add random word generator and words list

---
+++ Modules/random_word_generator.py
@@ -0,0 +1,18 @@
+import random
+
+
+def random_word_generator():
+    
+    """Function that returns a random word from the file 'mots.txt'."""
+
+    #Open the file 'mots.txt' in read mode
+    with open("mots.txt", "r") as f:  
+
+        #Read all lines from the file and create a list of words
+        word = f.read().splitlines()
+
+    # Return a word randomly chosen from the list
+    return random.choice(word)
+
+random_word_generator()
+
