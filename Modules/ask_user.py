f9653635032b8a9e8d0d0aec2053894e5618bbee
Hykoo13 <haikmonossiandev@gmail.com>
2026-01-22 16:58:16 +0100

    Add ask_user function and score logic

---
+++ Modules/ask_user.py
@@ -0,0 +1,52 @@
+import pygame
+
+def ask_user(word, window, font, display_text):
+    """This function captures what the player types on the keyboard"""
+    pygame.key.set_repeat(400, 50) # Enable key repeat
+    pygame.event.clear() # Clear existing events
+    user_input=""
+    WHITE = (240, 240, 240)
+    BLACK = (20, 20, 20)
+
+    while True:
+        # Clear the screen
+        window.fill(WHITE)
+
+        # Display the hidden word in the center
+        text_surface = font.render(display_text, True, BLACK)
+        text_rect = text_surface.get_rect(center=(window.get_width()//2, window.get_height()//2))
+        window.blit(text_surface, text_rect)
+
+        # Show the user input at the top left
+        if user_input:
+            input_surface = font.render(user_input, True, BLACK)
+            input_rect = input_surface.get_rect(topleft=(20, 20))
+            window.blit(input_surface, input_rect)
+        
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            # Check if a keyboard key is pressed
+            if event.type == pygame.KEYDOWN:
+
+                # If the pressed key is Enter
+                if event.key == pygame.K_RETURN:
+                    if len(user_input) > 0: # Only confirm if not empty
+                        pygame.key.set_repeat(0) # Disable key repeat
+                        return user_input   # Return the word or letter typed
+                
+                # If the pressed key is Backspace
+                elif event.key == pygame.K_BACKSPACE:
+                    user_input = user_input[:-1] # Delete the last letter
+                
+                # Add the typed character to the text if it is a letter and not longer than the word
+                elif len(user_input) < len(word) and event.unicode.isalpha():
+                    user_input += event.unicode
+
+                else :
+                    print("Word is max size or not a letter")
+            
+            # Quit management
+            elif event.type == pygame.QUIT:
+                pygame.key.set_repeat(0) # Disable key repeat
+                return None
--- Modules/menu.py
+++ Modules/menu.py
@@ -2,14 +2,12 @@ import pygame
 from display_word import display_word
 from random_word_generator import random_word_generator
 from verify_letter import verify_letter
+from ask_user import ask_user
 import time
-
-
 
 pygame.init()
 window = pygame.display.set_mode((680, 440))
 pygame.display.set_caption("Hangman Game")
-
 
 WHITE = (240,240,240)
 BLACK = (20,20,20)
@@ -22,13 +20,13 @@ buttons = [
     {"rect": pygame.Rect(240, 200, 200, 60), "text": "Quit", "action":"quit_game"}, 
 ]
 
-def start_game():
+def start_game(score):
     try:
         pv=7
         word = random_word_generator() # Generate a random word
         print(word) # log for debug
         letter_finds = [] 
-
+        
         playing = True
 
         while playing:
@@ -43,12 +41,25 @@ def start_game():
 
             pygame.display.flip()
 
-            user_input = "z" # ------------------- Replace ask user  -------------
+            user_input = ask_user(word, window, font, display_text)
+            if user_input is None: # if the user quit to prevent crashing
+                return score
 
-            if 1 < len(user_input) < len(word):
+            if len(user_input) == len(word):
                 if word == user_input: 
+                    score += 1
                     print("You win")
-                    return
+
+                    window.fill(WHITE) # Background color
+
+                    text_surface = font.render("You win", True, BLACK)
+                    text_rect = text_surface.get_rect(center=(window.get_width()//2, window.get_height()//2))
+                    window.blit(text_surface, text_rect)
+                    pygame.display.flip()
+                    time.sleep(3)
+                    playing = False
+
+                    return score
                 else:
                     pv-=1
             elif len(user_input) == 1:
@@ -68,11 +79,10 @@ def start_game():
                 text_rect = text_surface.get_rect(center=(window.get_width()//2, window.get_height()//2))
                 window.blit(text_surface, text_rect)
                 pygame.display.flip()
-                time.sleep(5)
-                playing = False
+                time.sleep(3)
+                return score
 
             print(pv)
-            time.sleep(1)
 
     except Exception as e :
         print("Error - start_game -", e)
@@ -80,6 +90,7 @@ def start_game():
 
 def menu():
     try:
+        score = 0
         running = True
         while running:
             for event in pygame.event.get():
@@ -90,12 +101,17 @@ def menu():
                         if btn["rect"].collidepoint(event.pos):
                             if btn["action"] == "start_game": 
                                 print("Starting the game...") # call your game loop here 
-                                start_game()
+                                score = start_game(score)
                             elif btn["action"] == "quit_game": 
                                 print("Quitting...") 
                                 running = False
 
             window.fill((240, 240, 240))
+
+            # Display Score
+            score_surface = font.render(f"Score: {score}", True, BLACK)
+            score_rect = score_surface.get_rect(topleft=(10, 10))
+            window.blit(score_surface, score_rect)
 
             # Draw button 
             for btn in buttons: 
